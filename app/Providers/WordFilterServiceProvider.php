0257549ac2c2131650a871c70f54888ff7ffdbb0
Joshua Bedford <joshua@JoshuaBedfordiMac.local>
2019-03-05 16:15:39 -0600

    added migrations to publish

--- app/Providers/WordFilterServiceProvider.php
+++ app/Providers/WordFilterServiceProvider.php
@@ -35,6 +35,10 @@ class WordFilterServiceProvider extends ServiceProvider
           realpath(__DIR__.'/../../config/word.php') => config_path('word.php'),
         ], 'config');
 
+        $this->publishes([
+            __DIR__.'/../database/migrations/create_word_list_tables.php.stub' => $this->getMigrationFileName($filesystem),
+        ], 'migrations');
+
         app('validator')->extend('word', function ($attribute, $value, $parameters, $validator) {
             return app('wordFilter')->noProhibitedWords($value);
         });
